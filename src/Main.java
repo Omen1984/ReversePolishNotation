bb2736af8a4335c0272f68047acd3997e97798ad
Omen1984 <ivan.plehanov42@yandex.ru>
2022-02-17 16:21:47 +0500

    add Scanner and two Collection

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
+        Queue<Integer> numbers = new LinkedList<>();
+        Deque<String> sign = new ArrayDeque<>();
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите математическую формулу");
+
+        String input = scanner.nextLine();
+        
     }
 }
